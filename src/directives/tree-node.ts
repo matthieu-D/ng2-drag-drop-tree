4695a15913a7ad61d279362a36552d9e4c173857
Matthieu Drula <matthieu.drula.hs@gmail.com>
2016-07-30 20:01:05 -0500

    update version

--- src/directives/tree-node.ts
+++ src/directives/tree-node.ts
@@ -2,34 +2,38 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {Draggable,Droppable} from 'primeng/primeng';
 import {TreeManager} from '../services/tree-manager';
 import {NodeSearch} from '../pipes/node-search';
-import {XEditableText} from '../ng2-xeditable-text.directive';
+import {XEditableText} from '../ng2-xeditable-text/ng2-xeditable-text.directive';
 
 @Component({
   selector: 'tree-node',
   directives: [Draggable,Droppable, TreeNode, XEditableText],
   template: require('./tree-node.html'),
+  styles:[require('./tree-node.css')],
   pipes: [NodeSearch]
 })
 
 export class TreeNode{
   @Input() children;
+  @Input('custom-classes') customClasses;
   @Output() askedChildDeletion = new EventEmitter();
   expanded:Boolean;
   constructor(private treeManager:TreeManager) {}
 
   addNewNode(node) {
     node.subNodes = [...node.subNodes, this.treeManager.getNewNode()];
+    node.expanded = true;
   }
 
-  deleteNode(node) {
-    this.children.splice(this.children.indexOf(node), 1);
+  hasChevronDown(child){
+    return child.subNodes.length != 0 && child.expanded
   }
 
-  getFilteredText(){
-    this.treeManager.getFilteredText();
+  hasChevronRight(child) {
+    return child.subNodes.length != 0 && !child.expanded
   }
 
-  onDragStart(event,child){
+
+  onDragStart(event, child){
     this.treeManager.setSelectedNode(child);
   }
 
@@ -38,6 +42,14 @@ export class TreeNode{
       this.treeManager.setHasDropped(false)
       this.children.splice(this.children.indexOf(child), 1);
     }
+  }
+
+  deleteNode(node) {
+    this.children.splice(this.children.indexOf(node), 1);
+  }
+
+  getFilteredText(){
+    this.treeManager.getFilteredText();
   }
 
   onDrop(event,node){
